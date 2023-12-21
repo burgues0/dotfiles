086fcf1d784e35ba0503cd9cbcbc2a4e2bfe0452
Victor Palmeira <60895346+burgues0@users.noreply.github.com>
2023-12-21 00:45:12 +0000

    add: create distroset.py

---
+++ distroset.py
@@ -0,0 +1,9 @@
+# script structure
+
+# sudo pacman -S pipewire pipewire-alsa pipewire-pulse wireplumber
+# sudo pacman -Rns pulseaudio
+# systemctl --user --now disable pulseaudio.service pulseaudio.socket
+# systemctl --user mask pulseaudio
+# systemctl --user --now enable pipewire pipewire-pulse wireplumber
+#
+# https://www.maketecheasier.com/install-configure-pipewire-linux/
