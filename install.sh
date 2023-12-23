4a0573ced4cc0e876ebe454284a72e8904ef4f34
burgues0 <victordpp@outlook.com>
2023-12-23 14:27:31 -0300

    edit: create symlinks for firefox, hyprland and waybar && create test user in firefox

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-#creates symbolic links for the config files in the repository
+sudo pacman --noconfirm -Syu pipewire pipewire-alsa pipewire-pulse wireplumber waybar firefox
+
+firefox -CreateProfile test
+profile_dir="$(basename ~/.mozilla/firefox/*.test)"
+mkdir -p ~/.mozilla/firefox/$profile_dir/chrome
 
 ln -s ~/distroset/waybar/ ~/.config
+ln -s ~/distroset/firefox/user.js ~/.mozilla/firefox/*.test/
+ln -s ~/distroset/firefox/userChrome.css ~/.mozilla/firefox/*.test/chrome/
 ln -s ~/distroset/hypr/ ~/.config
