9eae9759302a24edf09fa3e99370bcfadeb3ac3a
burgues0 <victordpp@outlook.com>
2023-12-24 00:30:24 -0300

    add: add wallpaper dir & symlink

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 sudo pacman --noconfirm -Syu firefox waybar lf hyprpaper pipewire pipewire-alsa pipewire-pulse wireplumber
 
-#when done, replace "test" with $USER
+when done, replace "test" with $USER
 firefox -CreateProfile test
 profile_dir="$(basename ~/.mozilla/firefox/*.test)"
 mkdir -p ~/.mozilla/firefox/$profile_dir/chrome
@@ -11,3 +11,4 @@ ln -s ~/distroset/waybar/ ~/.config
 ln -s ~/distroset/firefox/user.js ~/.mozilla/firefox/*.test/
 ln -s ~/distroset/firefox/userChrome.css ~/.mozilla/firefox/*.test/chrome/
 ln -s ~/distroset/hypr/ ~/.config
+ln -s ~/distroset/wallpapers/ ~
