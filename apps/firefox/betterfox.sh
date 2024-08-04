faee513f4a57ce570ea83691afe0dc9567561ca6
burgues0 <victordpp@outlook.com>
2024-08-04 00:35:35 -0300

    edit: temp fix for firefox automatic white theme

--- apps/firefox/betterfox.sh
+++ apps/firefox/betterfox.sh
@@ -12,6 +12,7 @@ wget https://raw.githubusercontent.com/yokoffing/Betterfox/main/user.js
 
 echo 'user_pref("browser.ctrlTab.sortByRecentlyUsed", true);' >> user.js
 echo 'user_pref("browser.startup.page", 3);' >> user.js
+echo 'user_pref("layout.css.prefers-color-scheme.content-override", 0);' >> user.js
 
 ln -s ~/dotfiles/apps/firefox/userChrome.css ~/.mozilla/firefox/*.$USER/chrome/
 ln -s ~/dotfiles/apps/firefox/user.js ~/.mozilla/firefox/*.$USER/
